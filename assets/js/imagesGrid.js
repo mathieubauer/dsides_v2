67e7352cdd360cfd69d5ca3efcacd906d7c6cac2
Alprod <41442860+Alprod@users.noreply.github.com>
2021-12-22 17:46:48 +0100

    ajout new package image grid
    Ajout d'un nouveau package pour un rendu des images en grid plus sympas

--- assets/js/imagesGrid.js
+++ assets/js/imagesGrid.js
@@ -1,1 +1,22 @@
 console.log('images gird')
+const Masonry = require('masonry-layout');
+let jQueryBridget = require('jquery-bridget');
+let imagesloaded = require('imagesloaded');
+jQueryBridget('masonry',Masonry,$);
+
+let $imgGrid = $('.imgGrid').masonry({
+        itemSelector: '.imgGrid-item',
+        columnWidth: 200,
+        fitWidth:true,
+        transitionDuration: '0.3s',
+        stagger: 20,
+        percentPosition: true
+});
+imagesloaded('.imgGrid-item', function () { $imgGrid.masonry('layout') });
+
+
+
+$('.imgGrid-item').click( function() {
+    $( this ).toggleClass('grid-item--gigante');
+    $imgGrid.masonry('layout');
+});
