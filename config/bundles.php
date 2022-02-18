489d7fbeaf9067f1bc141fbe34000cd9ec0352e1
Alprod <41442860+Alprod@users.noreply.github.com>
2022-02-18 18:59:49 +0100

    ajout du package ckeditor

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
