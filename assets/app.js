11135d501798a66a2fc1536cc815548aaba3e360
dsides <41442860+Alprod@users.noreply.github.com>
2022-07-15 14:49:01 +0200

    refactoring

--- assets/app.js
+++ assets/app.js
@@ -5,11 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
+// any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
 import 'bootstrap';
 
-// start the Stimulus application
+
 import './bootstrap';
 global.$ = global.jQuery = $;
 
