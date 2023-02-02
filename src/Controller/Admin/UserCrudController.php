091a4ba1cfef32ae225daf3971ff3cc32331cc0d
Alprod <101025650+dsidesdev@users.noreply.github.com>
2023-02-02 11:40:21 +0100

    add new path image at user
    Ajout d'un chemin de dossier pour enregistrer les images portrait des utilisateurs et refactoriser le champ role pour une meilleur rendu

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,14 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -24,28 +28,61 @@ class UserCrudController extends AbstractCrudController
         return [
 
             EmailField::new('email'),
-            TextField::new('firstName'),
-            TextField::new('lastName'),
+            TextField::new('firstName', 'Nom du héro'),
+            TextField::new('lastName', 'Prénom du héro'),
+            TextField::new('imageFile', 'Portrait du Héro')
+                     ->setFormType(VichImageType::class)
+                     ->hideOnIndex()
+                     ->hideOnDetail()
+                     ->setHelp('Portrait du héro Dsides'),
+
+            ImageField::new('image','Portrait Héro')
+                      ->setBasePath('/uploads/images/teams')
+                      ->setUploadDir('public/uploads/images/teams')
+                      ->onlyOnIndex()
+                      ->setSortable(false),
+
+            ImageField::new('image','Portrait Héro')
+                      ->setTemplatePath('/admin/user/image.html.twig')
+                      ->setHelp('Portrait du héro Dsides')
+                      ->onlyOnDetail(),
             TextEditorField::new('content'),
-
-            // TextField::new('imageFile')
-            //      ->setFormType(VichImageType::class)
-            //      ->hideOnIndex(),
-            // ImageField::new('image')
-            //      ->setBasePath('/uploads/images/projects')
-            //      ->setUploadDir('public/uploads/images/projects')->onlyOnIndex()->setSortable(false),
-
             AssociationField::new('projects')
                      ->setFormTypeOptions(['choice_label' => 'name']),
 
-            TextField::new('slug')
-                     ->setPermission('ROLE_ADMIN'),
             TextField::new('password')
                      ->hideOnIndex()
-                     ->setFormType(PasswordType::class)
+                     ->hideOnDetail()
+                     ->hideWhenUpdating()
+                     ->setFormType(PasswordType::class),
+
+            ChoiceField::new('roles')
+                       ->setChoices([
+	                                    'Utilisateur' => 'ROLE_USER',
+	                                    'Administrateur' => 'ROLE_ADMIN'
+                                    ])
+                       ->allowMultipleChoices()
+                       ->renderAsBadges()
+                       ->setPermission('ROLE_ADMIN'),
+
+            TextField::new('slug')
                      ->setPermission('ROLE_ADMIN'),
-            ArrayField::new('roles')
-                      ->setPermission('ROLE_ADMIN'),
         ];
     }
+
+	public function configureActions(Actions $actions): Actions
+	{
+		return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+	}
+
+	public function configureCrud(Crud $crud): Crud
+	{
+		return $crud
+			->setEntityLabelInSingular('Héro Dsides')
+			->setEntityLabelInPlural('Héros Dsides')
+			->setPageTitle('edit',
+				fn (User $user) => sprintf('Mise à jour de <b>%s</b>', $user->getFullname()))
+			->setPageTitle('detail',
+				fn (User $user) => sprintf('Fiche detail de <b>%s</b>', $user->getFullname()));
+	}
 }
