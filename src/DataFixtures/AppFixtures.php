791c17557bb87dc9f617d0c37e7d3dec43fe4ea2
Alprod <101025650+dsidesdev@users.noreply.github.com>
2023-02-02 11:20:45 +0100

    Modification tools fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,9 @@ use App\Entity\Category;
 use App\Entity\Client;
 use App\Entity\Project;
 use App\Entity\User;
+use App\Factory\CategoryFactory;
+use App\Factory\ClientFactory;
+use App\Factory\ProjectFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -15,49 +18,18 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-
-    private UserPasswordHasherInterface $encoder;
-
-    public function __construct(UserPasswordHasherInterface $encoder)
-    {
-        $this->encoder = $encoder;
-    }
-
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create('fr_FR');
-
-        // users
 	    UserFactory::createMany(10);
-
-	    // clients
-	    for ($j = 0; $j < 5; $j++) {
-		    $client = new Client();
-		    $client->setName($faker->company());
-		    $manager->persist($client);
-	    }
-
-	    // categories
-	    for ($k = 0; $k < 6; $k++) {
-		    $category = new Category();
-		    $category->setName($faker->word());
-		    $manager->persist($category);
-	        // projects
-		    for ($i = 0; $i < 6; $i++) {
-	            $project = new Project();
-	            $project
-	                ->setName($faker->words(3, true))
-	                ->setContent($faker->sentence())
-	                ->setDisplayOrder($i)
-	                ->setIsDisplayed($faker->boolean())
-	                ->setClient($client)
-	                ->addCategory($category)
-	                ->setImage('placeholder.jpg');
-	            $manager->persist($project);
-	        }
-	    }
-
-
-        $manager->flush();
+		ClientFactory::createMany(6);
+		CategoryFactory::createMany(6);
+		ProjectFactory::new()
+			->many(10)
+			->create(function (){
+				return [
+					'client' => ClientFactory::random(),
+					'category' => CategoryFactory::randomSet(1)
+				];
+			});
     }
 }
